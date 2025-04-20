6844d05d6ac20bc213d7464ec8bfb844db8ccb20
MariamZead <mariam.a.zead@gmail.com>
2025-04-20 13:54:55 +0200

    implement accordion toggle in 3 ways

--- Challenges/FAQ Accordion Component/assets/js/script.js
+++ Challenges/FAQ Accordion Component/assets/js/script.js
@@ -1,22 +1,77 @@
 "use strict";
 
-$(".accordion").click(function () {
-  // Toggle 'active' class on the clicked accordion
-  $(this).toggleClass("active");
+// ? 1st way (Basic For Loop)
+/*
+var allAccordions = document.querySelectorAll(".accordion");
+for (var i = 0; i < allAccordions.length; i++) {
+  // ! hyshof daymn akher wahed l2no laf wa khls
+  // var accordion = allAccordions[i];
+  // ! use an immediately invoked function expression (IIFE). This creates a new scope for each i
+  (function (index) {
+    allAccordions[index].addEventListener("click", function () {
+      var currentAccordion = allAccordions[index];
+      currentAccordion.classList.toggle("active");
+      var currentAccordionIcon =
+        currentAccordion.querySelector(".accordion-icon");
 
-  // Remove 'active' class from sibling accordions
+      if (currentAccordion.classList.contains("active")) {
+        currentAccordionIcon.src = "assets/Images/icon-minus.svg";
+      } else {
+        currentAccordionIcon.src = "assets/Images/icon-plus.svg";
+      }
+
+      for (var i = 0; i < allAccordions.length; i++) {
+        if (allAccordions[i] !== currentAccordion) {
+          allAccordions[i].classList.remove("active");
+          var siblingIcon = allAccordions[i].querySelector(".accordion-icon");
+          siblingIcon.src = "assets/Images/icon-plus.svg";
+        }
+      }
+    });
+  })(i);
+}
+*/
+
+// ? 2nd way (ForEach Loop)
+/*
+const allAccordions = document.querySelectorAll(".accordion");
+
+allAccordions.forEach((accordion) => {
+  accordion.addEventListener("click", () => {
+    accordion.classList.toggle("active");
+
+    const toggleIcon = accordion.querySelector(".accordion-icon");
+    if (accordion.classList.contains("active")) {
+      toggleIcon.src = "assets/Images/icon-minus.svg";
+    } else {
+      toggleIcon.src = "assets/Images/icon-plus.svg";
+    }
+
+    const siblings = Array.from(accordion.parentNode.children).filter(
+      (sibling) => sibling !== accordion
+    );
+    siblings.forEach((sibling) => {
+      sibling.classList.remove("active");
+      const siblingIcon = sibling.querySelector(".accordion-icon");
+      if (siblingIcon) {
+        siblingIcon.src = "assets/Images/icon-plus.svg";
+      }
+    });
+  });
+});
+*/
+
+// ? 3rd way (JQuery)
+$(".accordion").click(function () {
+  $(this).toggleClass("active");
   $(this).siblings(".accordion").removeClass("active");
   $(this)
-    .siblings(".accordion") // Select sibling accordions
-    .find(".toggle-icon") // Find toggle icons within sibling accordions
-    .attr("src", "assets/Images/icon-plus.svg"); // Change the src attribute to the plus icon
-
-  // Toggle icon source based on accordion state
+    .siblings(".accordion")
+    .find(".toggle-icon")
+    .attr("src", "assets/Images/icon-plus.svg");
   if ($(this).hasClass("active")) {
-    // If the accordion is active, change the icon to minus
     $(this).find(".toggle-icon").attr("src", "assets/Images/icon-minus.svg");
   } else {
-    // If the accordion is not active, change the icon to plus
     $(this).find(".toggle-icon").attr("src", "assets/Images/icon-plus.svg");
   }
 });
